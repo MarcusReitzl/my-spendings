acc3532fcc389ccde103e9151c9a0fe1dd2a4d29
elpatzo <34568725+elpatzo@users.noreply.github.com>
2019-01-23 21:57:42 +0100

    v_2_7_7

--- src/app/server.service.ts
+++ src/app/server.service.ts
@@ -10,7 +10,6 @@ baseurl = 'http://localhost:3000/';
 httpOptions = {
   headers: new HttpHeaders({
     'Content-Type' : 'application/json',
-    
   })
 };
   
@@ -19,16 +18,20 @@ httpOptions = {
 
   constructor(private http: HttpClient) { }
 
-  initialRequest() { return this.http.get(this.baseurl + 'booking'); }
+  initialRequest() { return this.http.get(this.baseurl + 'booking', this.httpOptions); }
 
   putBookings(booking: Booking){ return this.http.post(this.baseurl + 'booking/save', booking, this.httpOptions); }
 
   login(data){ return this.http.post(this.baseurl + 'login', data, this.httpOptions); }
 
-  register(data){ return this.http.post(this.baseurl+'register', data, this.httpOptions); }
+  register(data){ return this.http.post(this.baseurl+ 'register', data, this.httpOptions); }
 
-  getCategories(){ return this.http.get(this.baseurl + 'categorie'); }
+  getCategories(){ return this.http.get(this.baseurl + 'categorie', this.httpOptions); }
 
-  getBudgets(){ return this.http.get(this.baseurl + 'budgets'); };
+  getBudgets(){ return this.http.get(this.baseurl + 'budgets', this.httpOptions); }
+
+  postCategorie(data){ return this.http.post(this.baseurl + 'categorie/save', data, this.httpOptions); }
+
+  postBudget(data){ return this.http.post(this.baseurl + 'budget/save', data, this.httpOptions); }
 
 }
