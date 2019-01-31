16a47278099b583379ed8e66c7d64aae71184ec2
elpatzo <34568725+elpatzo@users.noreply.github.com>
2019-02-01 00:00:36 +0100

    v_3_2_2

--- src/app/statistics/statistics.component.ts
+++ src/app/statistics/statistics.component.ts
@@ -3,6 +3,7 @@ import { Chart } from 'chart.js';
 import { BudgetService } from '../budget.service';
 import { CategorieService } from '../categorie.service';
 import { FilterService } from '../filter.service';
+import { BookingService } from '../booking.service';
 
 @Component({
   selector: 'app-statistics',
@@ -21,7 +22,8 @@ dataAvailable: any[] = [];
 
   constructor(private budgetService: BudgetService, 
     private categorieService: CategorieService,
-    private filterService: FilterService) { }
+    private filterService: FilterService,
+    private bookingService: BookingService) { }
 
   ngOnInit() {
 
@@ -61,14 +63,12 @@ dataAvailable: any[] = [];
       }
     );
     
-  this.budgetService.budgetChanged.subscribe(
-    (budgets:any[])=>{
-      
+    this.budgetService.budgetChanged.subscribe(
+      (budgets:any[])=>{
       this.prepareArray(this.budgets);
       this.chart.update();
       this.chart.render(); 
-    }
-  )
+    });
 
   this.categorieService.valueChanged.subscribe(
     ()=>{
@@ -79,105 +79,129 @@ dataAvailable: any[] = [];
   )
 }
 
- 
-    
   setFilter(fromDate, toDate, budgetInput){
+    let counter: number = 0;
+    let newLabels: string[] = [];
     let filteredArray: any[] = [];
-    filteredArray = this.filterService.filterArray(fromDate.value, toDate.value, 'unselected', this.budgets);
-    if(budgetInput === 'unselected'){
-  //   this.chart = new Chart(this.chartRef.nativeElement, {
-  //     type: 'bar',
-  //     data: {
-  //       labels: this.labels,
-  //       datasets:[
-  //         {data:this.dataAvailable, label: 'available', backgroundColor: '#FF8080', alpha: 0.5},
-  //         {data:this.dataUsed, label:'used', backgroundColor: '#8091FF', alpha: 0.5 }          
-  //       ],
-  //       options:{
+    if(budgetInput.value === 'unselected'){
+
+
+      if(fromDate.value !== "" || toDate.value !== ""){
+        console.log('only date');
         
-  //       scaleShowVerticalLines: false,
-  //       responsive: true
-  //     }
-  //   }
-  // });
-  // this.chart.update();
-} else {
-  console.log('else?');
-  let newLabels: string[] = [];
-  let filteredArray: any[] = [];
-  for(let budget of this.budgets){
-    if(budget.budgetName === budgetInput.value){ 
-      for(let categorie of budget.includedCategories){
-      newLabels.push(categorie.name);
-      filteredArray.push(categorie.amount);
+        let bookings = this.bookingService.getBookings();
+        bookings = this.filterService.filterArray(fromDate.value, toDate.value, "unselected", bookings)
+               
+        for(let budget of this.budgets){         
+          for(let categorie of budget.includedCategories){
+            counter = 0;
+            newLabels.push(categorie.name); 
+              for(let booking of bookings){
+                if(booking.kategorie === categorie.name){                   
+                  counter = counter + booking.value;                                     
+                }
+              }
+            filteredArray.push(counter);
+          }
+        }
       }
-    }
-  }
+    } else {
+      console.log('budget with date');
+
+      let counter: number = 0;
+
+      if(fromDate.value !== "" || toDate.value !== ""){
+        let bookings = this.bookingService.getBookings();
+        bookings = this.filterService.filterArray(fromDate.value, toDate.value, "unselected", bookings)  
+        console.log(bookings);
+        
+        for(let budget of this.budgets){
+          if(budget.budgetName === budgetInput.value){ 
+            for(let categorie of budget.includedCategories){
+              counter = 0;
+              newLabels.push(categorie.name); 
+              for(let booking of bookings){
+                if(booking.kategorie === categorie.name){
+                  counter = counter + booking.value;          
+                  console.log(counter);
+                  
+                }
+              }
+              filteredArray.push(counter);
+            }
+          }
+        }
+      } else {
+        console.log('just budget');
+        for(let budget of this.budgets){
+          if(budget.budgetName === budgetInput.value){ 
+            for(let categorie of budget.includedCategories){
+            newLabels.push(categorie.name);
+            filteredArray.push(categorie.amount);
+            }
+          }
+        }
+      }
+    } 
   this.chart.destroy();
   this.chart = new Chart(this.chartRef.nativeElement, {
-    type: 'bar',
-    data: {
-      labels: newLabels,
-      datasets:[
-        {data:filteredArray, 
-          backgroundColor: ['#FF8080', '#8091FF', ]}       
-      ],
-    },
-      options:{
-        scales: {
-          yAxes: [{
-            ticks:{
-              beginAtZero:true
-            }
-          }]
-        },
-      scaleShowVerticalLines: false,
-      responsive: true,
-      legend:{
-        display:false
-      }
+  type: 'bar',
+  data: {
+    labels: newLabels,
+    datasets:[
+      {data:filteredArray, 
+        backgroundColor: ['#FF8080', '#8091FF', ]}       
+    ],
+  },
+    options:{
+      scales: {
+        yAxes: [{
+          ticks:{
+            beginAtZero:true
+          }
+        }]
+      },
+    scaleShowVerticalLines: false,
+    responsive: true,
+    legend:{
+      display:false
     }
   }
-);
-
-}
-    this.prepareArray(filteredArray);
-    this.filtered = true;
+  });
     this.chart.update();
   }
   
   clearFilter(){
-   this.filtered = false;
-   this.prepareArray(this.budgets);
-   this.chart.destroy();
-   
-   this.chart = new Chart(this.chartRef.nativeElement, {
-    type: 'bar',
-    data: {
-      labels: this.labels,
-      datasets:[
-        {data:this.dataAvailable, label: 'verfügbar', backgroundColor: '#FF8080', alpha: 0.5},
-        {data:this.dataUsed, label:'verbraucht', backgroundColor: '#8091FF', alpha: 0.5 }          
-      ]
-    },
-      options:{
-        tooltips:{
-          mode:'label'
-        },
-        scales: {
-          yAxes: [{
-            ticks:{
-              beginAtZero:true
-            }
-          }]
-        },
-        scaleShowVerticalLines: false,
-        responsive: true
-      }
+    this.filtered = false;
+    this.prepareArray(this.budgets);
+    this.chart.destroy();
+    
+    this.chart = new Chart(this.chartRef.nativeElement, {
+      type: 'bar',
+      data: {
+        labels: this.labels,
+        datasets:[
+          {data:this.dataAvailable, label: 'verfügbar', backgroundColor: '#FF8080', alpha: 0.5},
+          {data:this.dataUsed, label:'verbraucht', backgroundColor: '#8091FF', alpha: 0.5 }          
+        ]
+      },
+        options:{
+          tooltips:{
+            mode:'label'
+          },
+          scales: {
+            yAxes: [{
+              ticks:{
+                beginAtZero:true
+              }
+            }]
+          },
+          scaleShowVerticalLines: false,
+          responsive: true
+        }
+    });
+    this.chart.update();
     }
-  );
-   this.chart.update();
-  }
 
   prepareArray(budgets: any[]){
     this.dataUsed = [];
@@ -194,7 +218,7 @@ dataAvailable: any[] = [];
       for(let include of budget.includedCategories){ 
         budgetUsedCounter = budgetUsedCounter + parseInt(include.amount);
       }
-     this.dataUsed.push(budgetUsedCounter);
+      this.dataUsed.push(budgetUsedCounter);
     }
   }
 }
